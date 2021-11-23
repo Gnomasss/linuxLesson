b7853449eb9ceff2042fbeb40e98897d7f410dd7
Gnomasss <woolf8844xz@gmail.com>
2021-11-23 10:13:50 +0300

    second task

---
+++ task2/userhome.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+unset fl_file
+unset user
+unset file
+
+for arg in "$@"
+do
+    if [[ "$arg" == "-f" ]]; then
+        fl_file=1
+    else
+        if [[ "$fl_file" && -z "$file" ]]; then
+            file=$arg
+        else
+            user=$arg
+        fi
+    fi
+
+done
+
+if [[ -z "$file" ]]; then
+    file="/etc/passwd"
+fi
+
+if [[ -z "$user" ]]; then
+    user=$USER
+fi
+
+if [[ ! -e "$file" ]]; then
+    echo "file does not exist" >$2
+    exit 2
+fi
+
+str="$(grep $user $file)"
+
+if [[ "$str" == "" ]]; then
+    echo "user does not exist" >$2
+    exit 1
+fi
+
+echo $str | cut -d ':' -f 6
+
+
